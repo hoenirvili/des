83b16419cb6b87e1aeadc444683d88a9676d7bfb
hoenirvili <hoenirvili@gmail.com>
2020-03-17 15:02:52 +0200

    Cut the bits after xor operation that we are not interested
    Signed-off-by: hoenirvili <hoenirvili@gmail.com>

--- des.c
+++ des.c
@@ -166,56 +166,56 @@ const uint8_t ebit_selection_table[48] = {
       28, 29, 30, 31, 32,  1
 };
 
-const uint8_t S1[4][16] = {
+const uint8_t S1[64] = {
    14, 4, 13,  1,  2, 15, 11,  8,  3, 10,  6, 12,  5,  9, 0, 7,
     0, 15,  7,  4, 14,  2, 13,  1, 10,  6, 12, 11,  9,  5,  3,  8,
     4,  1, 14,  8, 13,  6,  2, 11, 15, 12,  9,  7,  3, 10,  5,  0,
     15, 12,  8,  2,  4,  9,  1,  7,  5, 11,  3, 14, 10,  0,  6, 13
 };
 
-const uint8_t S2[4][16] = {
+const uint8_t S2[64] = {
     15,  1,  8, 14,  6, 11,  3,  4,  9,  7,  2, 13, 12,  0,  5, 10,
      3, 13,  4,  7, 15,  2,  8, 14, 12,  0,  1, 10,  6,  9, 11,  5,
      0, 14,  7, 11, 10,  4, 13,  1,  5,  8, 12,  6,  9,  3,  2, 15,
     13,  8, 10,  1,  3, 15,  4,  2, 11,  6,  7, 12,  0,  5, 14,  9
 };
 
-const uint8_t S3[4][16] = {
+const uint8_t S3[64] = {
     10,  0,  9, 14,  6,  3, 15,  5,  1, 13, 12,  7, 11,  4,  2,  8,
     13,  7,  0,  9,  3,  4,  6, 10,  2,  8,  5, 14, 12, 11, 15,  1,
     13,  6,  4,  9,  8, 15,  3,  0, 11,  1,  2, 12,  5, 10, 14,  7,
      1, 10, 13,  0,  6,  9,  8,  7,  4, 15, 14,  3, 11,  5,  2, 12
 };
 
-const uint8_t S4[4][16] = {
+const uint8_t S4[64] = {
      7, 13, 14,  3,  0,  6,  9, 10,  1,  2,  8,  5, 11, 12,  4, 15,
     13,  8, 11,  5,  6, 15,  0,  3,  4,  7,  2, 12,  1, 10, 14,  9,
     10,  6,  9,  0, 12, 11,  7, 13, 15,  1,  3, 14,  5,  2,  8,  4,
      3, 15,  0,  6, 10,  1, 13,  8,  9,  4,  5, 11, 12,  7,  2, 14
 };
 
-const uint8_t S5[4][16] = {
+const uint8_t S5[64] = {
      2, 12,  4,  1,  7, 10, 11,  6,  8,  5,  3, 15, 13,  0, 14,  9,
     14, 11,  2, 12,  4,  7, 13,  1,  5,  0, 15, 10,  3,  9,  8,  6,
      4,  2,  1, 11, 10, 13,  7,  8, 15,  9, 12,  5,  6,  3,  0, 14,
     11,  8, 12,  7,  1, 14,  2, 13,  6, 15,  0,  9, 10,  4,  5,  3
 };
 
-const uint8_t S6[4][16] = {
+const uint8_t S6[64] = {
     12,  1, 10, 15,  9,  2,  6,  8,  0, 13,  3,  4, 14,  7,  5, 11,
     10, 15,  4,  2,  7, 12,  9,  5,  6,  1, 13, 14,  0, 11,  3,  8,
      9, 14, 15,  5,  2,  8, 12,  3,  7,  0,  4, 10,  1, 13, 11,  6,
      4,  3,  2, 12,  9,  5, 15, 10, 11, 14,  1,  7,  6,  0,  8, 13
 };
 
-const uint8_t S7[4][16] = {
+const uint8_t S7[64] = {
      4, 11,  2, 14, 15,  0,  8, 13,  3, 12,  9,  7,  5, 10,  6,  1,
     13,  0, 11,  7,  4,  9,  1, 10, 14,  3,  5, 12,  2, 15,  8,  6,
      1,  4, 11, 13, 12,  3,  7, 14, 10, 15,  6,  8,  0,  5,  9,  2,
      6, 11, 13,  8,  1,  4, 10,  7,  9,  5,  0, 15, 14,  2,  3, 12
 };
 
-const uint8_t S8[4][16] = {
+const uint8_t S8[64] = {
     13,  2,  8,  4,  6, 15, 11,  1, 10,  9,  3, 14,  5,  0, 12,  7,
      1, 15, 13,  8, 10,  3,  7,  4, 12,  5,  6, 11,  0, 14,  9,  2,
      7, 11,  4,  1,  9, 12, 14,  2,  0,  6, 10, 13, 15,  3,  5,  8,
@@ -223,20 +223,21 @@ const uint8_t S8[4][16] = {
 };
 
 
-static uint64_t expand_block(uint32_t block, const uint8_t* pc)
+static uint64_t expand_block(uint64_t block, const uint8_t* pc)
 {
     uint64_t expand = 0;
-    const size_t size = sizeof(ebit_selection_table) * 8;
-    for (size_t i = 0; i < size; i++)
+    for (size_t i = 0; i < 48; i++)
         expand |= (block >> (32 - pc[i]) & 0x1) << (48 - 1 - i);
     return expand;
 
 }
-static uint32_t f(uint32_t block, key key)
+
+static uint64_t f(uint32_t block, key key)
 {
+    assert(ARRAY_SIZE(ebit_selection_table) == 48);
     uint64_t eblock = expand_block(block, ebit_selection_table);
-    return  eblock ^ key;
-    //TODO(hoenir): finish this
+    assert(eblock == 0x7A15557A1555);
+    return  (eblock ^ key) & 0xFFFFFFFFFFFF; // we are only interested of the 48 bits
 }
 
 
@@ -265,6 +266,8 @@ int des_encrypt(key inkey, const char *input)
     // this is for testing purposes
     uint64_t m = 0x123456789ABCDEF;
     m = initial_permutation(m, IP);
+    assert(m == 0xCC00CCFFF0AAF0AA);
+
     /* size_t n = strlen(padin); */
     /* for (size_t i = 0; i < n; i = i + sizeof(m)) { */
     /*     uint64_t m = 0; */
@@ -272,13 +275,17 @@ int des_encrypt(key inkey, const char *input)
     /*     m = initial_permutation(m, IP); */
     /*     memcpy(&padin[i], &m, sizeof(m)); */
     /* } */
+
     uint32_t l = (m >> 32);
     uint32_t r = m;
     uint32_t aux_l = 0;
+
     for (size_t i = 0; i < ROUNDS; i++) {
         aux_l = r;
-        r = l ^ f(r, keys[i]);
+        uint64_t newr = (l ^ f(r, keys[i])) & 0xFFFFFFFFFFFF;
+        assert(newr == 0x6117BA866527);
         l = aux_l;
+        break;
     }
 
     return EXIT_SUCCESS;
